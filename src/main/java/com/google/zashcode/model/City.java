67eeafae64095cc53d98a04cace98ca5bc475f3d
recuealv <alvaro.recuenco@adidas-group.com>
2018-03-01 19:03:20 +0100

    Model

---
+++ src/main/java/com/google/zashcode/model/City.java
@@ -0,0 +1,17 @@
+package com.google.zashcode.model;
+
+import java.util.List;
+
+public class City {
+
+    private Integer columns;
+
+    private Integer rows;
+
+    private List<Car> cars;
+
+    private List<Ride> rides;
+
+    private Integer bonus;
+    
+}
---
+++ src/main/java/com/google/zashcode/model/Ride.java
@@ -0,0 +1,4 @@
+package com.google.zashcode.model;
+
+public class Ride {
+}
