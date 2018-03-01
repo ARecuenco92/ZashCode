6338bf5e103e7fb4b29cd97581fec88c808f905d
recuealv <alvaro.recuenco@adidas-group.com>
2018-03-01 19:24:50 +0100

    Applciation

--- src/main/java/com/google/zashcode/Application.java
+++ src/main/java/com/google/zashcode/Application.java
@@ -1,9 +1,26 @@
 package com.google.zashcode;
 
+import com.google.zashcode.model.City;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args){
+        List<String> files = new ArrayList<String>();
+        files.add("a_example");
+        files.add("b_should_be_easy");
+        files.add("c_no_hurry");
+        files.add("d_metropolis");
+        files.add("e_high_bonus");
 
+        City city = null;
+        for (String file : files) {
+            //city = DataLoader;
+
+            DataOutput.write(file, city.drive());
+        }
     }
 
 }
